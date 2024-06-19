1637dc4cd7a514759015fa9213127089030bd367
Samuele <samuele.lattanzi@stud.unifi.it>
2024-06-19 16:17:04 +0200

    third-commit

--- musicstreamingservice/settings.py
+++ musicstreamingservice/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kud*53=341(7%rtm#efh)d&dt@&b5l&(o19+5mtj7n=0a2jsqo'
-#SECRET_KEY = os.environ.get("SECRET_KEY")
+#SECRET_KEY = 'django-insecure-kud*53=341(7%rtm#efh)d&dt@&b5l&(o19+5mtj7n=0a2jsqo'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", '') == "True"
 
 ALLOWED_HOSTS = []
 
