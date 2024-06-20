67d03d993de9b6ca1fb5c7e9bbd563c451f947c6
Samuele <samuele.lattanzi@stud.unifi.it>
2024-06-20 13:12:31 +0200

    final-commit-2

--- musicstreamingservice/settings.py
+++ musicstreamingservice/settings.py
@@ -133,6 +133,12 @@ LOGIN_URL = '/login/'
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 
 
